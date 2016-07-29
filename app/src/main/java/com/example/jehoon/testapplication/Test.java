d534137b7a84decb5532a9719ca17fee7234653e
jehoon <eden1948@nate.com>
2016-07-29 11:55:11 +0900

    첫번째 Commit

---
+++ app/src/main/java/com/example/jehoon/testapplication/Test.java
@@ -0,0 +1,10 @@
+package com.example.jehoon.testapplication;
+
+/**
+ * Created by jehoon on 2016-07-29.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.print("안녕하세요");
+    }
+}
